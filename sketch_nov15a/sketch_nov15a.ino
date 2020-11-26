5cf69999d6d300413c5a42d57679b5652577079f
joellenhq <73105482+joellenhq@users.noreply.github.com>
2020-11-26 01:59:43 +0100

    Początkowe prace nad algorrytmem optymalnym

--- sketch_nov15a/sketch_nov15a.ino
+++ sketch_nov15a/sketch_nov15a.ino
@@ -4,14 +4,25 @@ int x=16,y=16;
 //zmienna okreslajaca pozycje koncowa
 int pozKoncowa=x*y-1;
 //zmienna okreslajaca pozycje na mapie
-int aktpole; 
+int poleStart=0;
+int aktpole=0; 
 //tablice pozwalajace okreslic polozenie scian wokol kazdego pola
 boolean n[255],e[255],s[255],w[255];
 //byte kierunki[255];
 //zdefiniowanie tablicy, ktora bedzie zawierac wszystkie pola
 boolean pole[255];
-//zalozenie, ze zaczynamy w lewym dolnym rogu, konczymy w prawym gornym
+//poczatkowe ustawienie robota (orientacja w terenie) to 0 stopni (robot ustawiony w kierunku N labiryntu
+int konfiguracja=0;
+//kierunek ruchu
+int kierunek;
+//tablica, do ktorej zostana wpisane pola, po ktorych robot ma sie poruszac podczas algorytmu zoptymalizowanego 
+int polaRuchu[255];
+//tablica, do ktorej wpisywane sa wagi pol wg przyspieszonego algorytmu propagacji fali
+int waga[255];
 
+//zmienna okreslajaca granice okreslajaca czy sciana jest w obrebie aktualnego pola
+int maxOdl=15;
+//zalozenie, ze zaczynamy w lewym dolnym rogu, konczymy w prawym gornym
 
 void lewo(){
   
@@ -82,7 +93,7 @@ void pomiar(int konfiguracja, int i){
   }
     
   
-  if(sensor1<15){ 
+  if(sensor1<maxOdl){ 
     //wartosc 0 oznacza, ze mozliwy jest ruch w tamtym kierunku (nie ma sciany), a 1 oznacza przeszkode 
     n[i]=1;
     //jesli sciana znajduje sie po jednej stronie komorki to odpowiednio po drugiej stronie sciany jest inna komorka 
@@ -95,7 +106,7 @@ void pomiar(int konfiguracja, int i){
     if(i>=(x-1)*y && i<=x*y-1);
     else s[i+y]=0;
   }
-  if(sensor2<15){
+  if(sensor2<maxOdl){
     e[i]=1;
     if((i+1)%y==0);
     else w[i+1]=1;
@@ -105,10 +116,10 @@ void pomiar(int konfiguracja, int i){
     if((i+1)%y==0);
     else w[i+1]=0;
   }
-  if(sensor3<15){
+  if(sensor3<maxOdl){
     s[i]=1;
     if(i>=0 && i<y);
-    else n[i-y]=1
+    else n[i-y]=1;
   }
   else{
     s[i]=0;
@@ -117,7 +128,7 @@ void pomiar(int konfiguracja, int i){
     //jesli labirynt jest otwarty na poczatku to niemozliwe ma byc wyjechanie z niego
     if(i==0) s[1]=1;
   }
-  if(sensor4<15){
+  if(sensor4<maxOdl){
     w[i]=1;
     if(i%y==0);
     else e[i-1]=1;
@@ -130,53 +141,58 @@ void pomiar(int konfiguracja, int i){
   }
 }
 
+//funkcja sprawdzajaca mozliwy ruch i wybierajaca kierunek ruchu przy algorytmie dogłębnego przeszukiwania
 int sprawdzenie(int i){
   int a=1;
-  while(1){
- 
-     
-      if(n[i]==1 && pole[i+y]==0){
+  
+      //jesli nie ma sciany i pole nie bylo wczesnniej zbadane to wybierze ten kierunek ruchu
+      //priorytet wedlug rozy wiatrow NESW
+      if(n[i]==0 && pole[i+y]==0){
         return 1;
       }
-      else if(e[i]==1 && pole[i+1]==0){
+      else if(e[i]==0 && pole[i+1]==0){
         return 2;
       }
-      else if(s[i]==1 && pole[i-y]==0){
+      else if(s[i]==0 && pole[i-y]==0){
         return 3;
       }
-      else if(w[i]==1 && pole[i-1]==0){
+      else if(w[i]==0 && pole[i-1]==0){
         return 4;
       }
+      //jesli wszystkie pola zostaly zbadane to zostanie wybrany losowo kierunek tam gdzie nie ma sciany
+      //kierunek bedzie losowany do skutku, az zostanie znaleziona mozliwa droga ruchu
       else{
-        los= random(1,4);
-    switch(los) {
-      case 1:
-      if(n[i]==1){
-        return 1;
+        while(1){
+          los= random(1,4);
+            switch(los) {
+              case 1:
+                if(n[i]==0){
+                  return 1;
+                }
+              break;
+              case 2:
+                if(e[i]==0){
+                  return 2;
+                }
+              break;
+              case 3:
+                if(s[i]==0){
+                  return 3;
+                }
+              break;
+              case 4:
+                if(w[i]==0){
+                  return 4;
+                }
+              break;
+          }
       }
-      break;
-      case 2:
-      if(e[i]==1){
-        return 2;
-      }
-      break;
-      case 3:
-      if(s[i]==1){
-        return 3;
-      }
-      break;
-      case 4:
-      if(w[i]==1){
-        return 4;
-      }
-      break;
-  }
     }
-  }
 }
 
-void konfiguracja1(int konfiguracja, int i){
-  int kierunek=sprawdzenie(i);
+//funkcja, ktora pozwala na wykonanie ruchu jesli znane sa obecna orientacja robota w terenie, kierunek dalszego ruchu oraz aktualne pole
+void ruch(int konfiguracja, int kierunek, int i){
+  
   if(konfiguracja==0){
     if(kierunek==1){
       prosto();
@@ -267,14 +283,54 @@ void konfiguracja1(int konfiguracja, int i){
   }
 }
 
+void wagi(int orientacja, int i){
+    int orientacja1=orientacja;
+    if(n[i]==0){
+        if(orientacja1==0 || orientacja1==180){
+          if(waga[i+y]>waga[i]+1) waga[i+y]=waga[i]+1;
+        }
+        else{ 
+          if(waga[i+y]>waga[i]+3) waga[i+y]=waga[i]+3;
+        }
+     }
+      
+     if(e[i]==0){
+        if(orientacja1==0 || orientacja1==180)
+          if(waga[i+y]>waga[i]+3) waga[i+y]=waga[i]+3;
+        else{
+          if(waga[i+y]>waga[i]+1) waga[i+y]=waga[i]+1;
+        }
+     }
+     if(s[i]==0){
+        if(orientacja1==0 || orientacja1==180)
+          if(waga[i+y]>waga[i]+1) waga[i+y]=waga[i]+1;
+        else{
+          if(waga[i+y]>waga[i]+3) waga[i+y]=waga[i]+3;
+        }
+     }
+     if(w[i]==0){
+        if(orientacja1==0 || orientacja1==180)
+          if(waga[i+y]>waga[i]+3) waga[i+y]=waga[i]+3;
+        else{
+          if(waga[i+y]>waga[i]+1) waga[i+y]=waga[i]+1;
+        }
+     }
+}
+
+void sciezka{
+  int i=0;
+  tablica[i]=pozKoncowa;
+  pozycja=pozKoncowa;
+  while(pozycja!=poleStart){
+    //wpisujemy do tablicy kolejno pola o mniejszych wagach, jesli wagi sa takie same to wybieramy sciezke wzgledem poprzedniej wartosci, jesli nadal takie same to losujemy
+    //sprawdzamy pola wokol, od najmniejszej wagi i wybieramy tam gdzie mozliwy jest ruch
+    //
+  }
+}
+
 void setup() {
-  //zmienna sprawdzająca czy wszystkie pola zostały sprawdzone
+  //zmienna, ktora posluzy do zbadania czy wszystkie pola zostały sprawdzone
   int j=1;
-  //zmienna okreslajaca pozycje na mapie jest ustawiana na 0 (pole poczatkowe)
-  aktpole=0;
-  
-  //konfiguracja robota w przestrzeni wyrazana w stopniach
-  int konfiguracja=0;
   
   //petla zotanie wykonywana dopoki nie zostana zbadane wszystkie pola
   while(j!=0){
@@ -284,6 +340,7 @@ void setup() {
     if(pole[aktpole]=0){
       pomiar(konfiguracja,aktpole);
       pole[aktpole]=1;
+      //gdy pole zostaje zbadane to dokonane zostaje sprawdzenie czterech sasiednich pol czy polozenie scian nie jest wiadome poprzez znajomosc otaczajacych ich pol
       if(pole[aktpole+2*y]==1 && pole[aktpole+y-1]==1 && pole[aktpole+y+1]==1){
         pole[aktpole+y]=1;  
       }
@@ -297,19 +354,32 @@ void setup() {
         pole[aktpole-1]=1;  
       }
     }
-
+    //wywolanie funkcji wybierajacej kierunek ruchu dla algorytmu eksploracyjnego (doglebnego przeszukiwania)
+    int kierunek=sprawdzenie(aktpole);
     //wywolanie funkcji powodujacej ruch mechanizmu
-    konfiguracja1(konfiguracja, aktpole);
-    //zastosowanie algorytmu siłowego do eksploracji labiryntu
-    //nie wiem czy to silowy, ale wydaje sie ok
-    
+    ruch(konfiguracja,kierunek,aktpole);
     //ustawienie licznika niezbadanych pol na zero
     j=0;
     //sprawdzenie ile pol zostalo niezbadanych
     for(int k=0;k<255;k++){
       if(pole[k]==0) j++;  
     }
-  
+  }
+  //po zakonczeniu algorytmu robot powinien ustawic sie na pole startowe
+  //no coz
+  //wpisanie do tablic z wagami najwiekszej mozliwej wartosci
+  for(int i=0;i<=pozKoncowa;i++){
+    waga[i]=pozKoncowa;
+  }
+  if(aktpole=0){
+    //nadanie polu poczatkowemu wagi 1
+    waga[aktpole]=1;
+    //zdefiniowanie zmiennej, ktora przysluzy do sprawdzenia wag calego labiryntu
+    int badanepole=aktpole;
+    //wpisanie wag sasiednim polom wedlug konfiguracji (ruch przod/tyl waga++, ruch wymagajacy skretu waga+3)
+    wagi(konfiguracja,badanepole);
+
+    //
   }
 }
 
